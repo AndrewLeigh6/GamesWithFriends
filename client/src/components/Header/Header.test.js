1a682cdd6e71b4609f7074a9d6a5c25c4ef43aee
Andrew <andrewleigh6@gmail.com>
2020-11-24 14:25:05 +0000

    Remove old client

--- client/src/components/Header/Header.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Header from "./Header";
-
-it("renders heading and subheading correctly", () => {
-  const { getByText } = render(<Header />);
-  const text = getByText(/games with friends/i);
-  expect(text).toBeInTheDocument();
-});
