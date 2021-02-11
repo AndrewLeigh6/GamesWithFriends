4ce4b7ecffaf38516431b7089e0789009cc68c05
Andrew <andrewleigh6@gmail.com>
2021-02-11 13:38:53 +0000

    Add additional input tests

--- client/src/components/Input/Input.test.tsx
+++ client/src/components/Input/Input.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Input, { LeftIcon, RightIcon } from "./Input";
+import userEvent from "@testing-library/user-event";
 
 it("renders input correctly with props", () => {
   const component = (
@@ -17,11 +18,13 @@ it("renders input correctly with props", () => {
   getByText(/your steam url/i);
   getByRole("textbox", { name: /your steam url/i });
 
-  const button = queryByRole("button", { name: /remove/i });
+  const button = queryByRole("button", { name: /remove button/i });
   expect(button).not.toBeInTheDocument();
 });
 
-it("renders input correctly with right icon", () => {
+it("renders input correctly with right icon props", () => {
+  const clicked = jest.fn();
+
   const component = (
     <Input
       label="Your Steam URL"
@@ -29,6 +32,7 @@ it("renders input correctly with right icon", () => {
       placeholder="Enter your Steam URL"
       leftIcon={LeftIcon.User}
       rightIcon={RightIcon.Times}
+      iconClicked={clicked}
     />
   );
 
@@ -36,5 +40,29 @@ it("renders input correctly with right icon", () => {
 
   getByText(/your steam url/i);
   getByRole("textbox", { name: /your steam url/i });
-  getByRole("button", { name: /remove/i });
+  const button = getByRole("button", { name: /remove button/i });
+
+  userEvent.click(button);
+  expect(clicked).toHaveBeenCalled();
+});
+
+it("renders input correctly with value prop and copy icon", () => {
+  const component = (
+    <Input
+      label="felineyx"
+      name="felineyx"
+      value="http://localhost:3000/session?q=PrivateChocolateWolverine"
+      leftIcon={LeftIcon.User}
+      rightIcon={RightIcon.Copy}
+      iconClicked={() => null}
+    />
+  );
+
+  const { getByRole, getByDisplayValue } = render(component);
+
+  getByRole("textbox", { name: /felineyx/i });
+  getByRole("button", { name: /copy button/i });
+  getByDisplayValue(
+    "http://localhost:3000/session?q=PrivateChocolateWolverine"
+  );
 });
