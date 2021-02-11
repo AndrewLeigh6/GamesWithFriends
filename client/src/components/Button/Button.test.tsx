2d8725bc4d238b19024518182a75b593d3b2bb3f
Andrew <andrewleigh6@gmail.com>
2021-02-11 13:45:59 +0000

    Add additional Button test

--- client/src/components/Button/Button.test.tsx
+++ client/src/components/Button/Button.test.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Button from "./Button";
+import userEvent from "@testing-library/user-event";
 
 it("renders button correctly with props", () => {
   const component = <Button color="Primary">Select</Button>;
@@ -8,4 +9,18 @@ it("renders button correctly with props", () => {
   const { getByRole } = render(component);
 
   getByRole("button", { name: /select/i });
+});
+
+it("fires the click event correctly", () => {
+  const clicked = jest.fn();
+  const component = (
+    <Button color="Primary" clicked={clicked}>
+      Select
+    </Button>
+  );
+  const { getByText } = render(component);
+  const button = getByText("Select");
+  expect(clicked).not.toHaveBeenCalled();
+  userEvent.click(button);
+  expect(clicked).toHaveBeenCalledTimes(1);
 });
